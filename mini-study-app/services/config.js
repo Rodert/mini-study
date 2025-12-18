2b67e4e36bd584e56a1e8f86dee9df40d6f88dd4
javapub <iswangshiyu@foxmail.com>
2025-12-18 20:47:04 +0800

    dockerfile

--- mini-study-app/services/config.js
+++ mini-study-app/services/config.js
@@ -3,7 +3,8 @@
 const USE_MOCK = false; // 设置为 true 使用 mock 数据， false 使用真实 API
 
 // API 基础地址配置
-const API_BASE_URL = 'http://localhost:8080/api/v1'; // 根据实际环境修改
+// const API_BASE_URL = 'http://localhost:8080/api/v1'; // 根据实际环境修改
+const API_BASE_URL = 'https://enterprise-learn.javapub.net.cn/api/v1'; // 根据实际环境修改
 
 module.exports = {
   USE_MOCK,
